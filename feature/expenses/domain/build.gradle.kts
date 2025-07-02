c8eed03018f90d7b56b682e0424a4d9593432f0f
Isaac Serrano <serranoie99@gmail.com>
2025-07-02 11:16:47 -0700

    feat: add data and domain modules for expenses feature

---
+++ feature/expenses/domain/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+
+    kotlin("plugin.serialization") version "2.1.21"
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+dependencies {
+    implementation(project(":core:domain"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+}
